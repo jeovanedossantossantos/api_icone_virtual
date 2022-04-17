9975f7f1049b3f574c68504d9c6f85a4e58e37fa
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-16 21:11:15 -0300

    feat: create auth controller

---
+++ src/controllers/controllerAuth.js
@@ -0,0 +1,42 @@
+const {User} = require('../models')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = require('../config/secret')
+const controllerAuth = {
+    async login(req, res){
+        const {email, senha} = req.body;
+        // try {
+            const usuario = await User.findOne({
+                where: {
+                    email: email
+                },
+            });
+            // res.status(200).json(usuario)
+    
+       
+        if(!usuario){
+            return res.status(400).json({"messagem": "E-mail não cadastrado ou Senha invalida!"})
+              
+        }
+        if(!bcrypt.compareSync(senha, usuario.senha)){
+            return res.status(401).json({"messagem": "E-mail não cadastrado ou Senha invalida!"})
+        }
+    
+
+        const tokem = jwt.sign({
+            id: usuario.id,
+            email: usuario.email,
+            nome: usuario.nome,
+            userType: "user"
+
+        }, secret.key)
+        return res.json({"tokem":tokem})
+    // }catch(e){
+    //     res.status(e)
+    // }
+
+
+    }
+}
+
+module.exports = controllerAuth;
