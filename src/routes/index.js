de146689591e11a119a38035a73952911e7b1c9c
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-18 19:35:06 -0300

    refactor: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,38 @@
-const express = require('express')
-const controllerUser = require("../controllers/controllerUser")
-const controllerAuth = require("../controllers/controllerAuth")
+const express = require('express');
+const FavoritoController = require('../app/controllers/FavoritoController');
+const ProdutoController  = require('../app/controllers/ProdutoController');
+const SessionController = require('../app/controllers/SessionController');
+const UserController = require('../app/controllers/UserController');
+const auth = require('../middlewares/auth');
+const root = require('../middlewares/root');
+
 const routes = express.Router();
 
 routes.get('/',(req,res) => {
     res.send("Olá mundo!")
 })
+routes.post('/session', SessionController.create)
 
-routes.post('/login',controllerAuth.login)
+routes.post('/users', UserController.create)
+routes.get('/users/:id', UserController.index)
+
+routes.delete('/users/:id', UserController.delete)
+routes.put('/users/:id', UserController.update)
+
+routes.get('/products/:id', ProdutoController.index)
+routes.get('/products', ProdutoController.list)
 
 
-routes.post('/sigin',controllerUser.register)
-routes.post('/like',controllerUser.like)
-routes.delete('/:id',controllerUser.delete)
-routes.put('/:id',controllerUser.updateUser)
-routes.get('/list-users',controllerUser.getAll)
-routes.get('/user-one/:id',controllerUser.getOne)
+routes.use(auth)
+routes.post('/likes/:produto_id', FavoritoController.create)
+routes.delete('/likes/:id', FavoritoController.delete)
+
+routes.use(root)
+routes.get('/users', UserController.list)
+routes.post('/products', ProdutoController.create)
+routes.put('/products/:id', ProdutoController.update)
+routes.delete('/products/:id', ProdutoController.delete)
+
+
+
 module.exports = routes
