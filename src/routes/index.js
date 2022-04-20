9be12d2e75a0356518b2c30f40796287501706b8
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-20 20:03:15 -0300

    feat: Image upload route adjustments

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const root = require('../middlewares/root');
 const multer = require('multer');
 const {storageProdutos, storageUser} = require('../config/multer');
 const upload = require('../middlewares/upload');
+const ImagensController = require('../app/controllers/ImagensController');
 const parserUser = multer({storage:storageUser});
 const parserProdutos = multer({storage:storageProdutos})
 
@@ -17,35 +18,30 @@ routes.get('/',(req,res) => {
     res.send("Olá mundo!")
 })
 routes.post('/session', SessionController.create)
-
-
-routes.post('/users', upload, UserController.create)
-
+routes.post('/users', UserController.create)
 routes.get('/users/:id', UserController.index)
-
-routes.delete('/users/:id', UserController.delete)
-routes.put('/users/:id',parserUser.single('img_perfil'), UserController.update)
-
 routes.get('/products/:id', ProdutoController.index)
 routes.get('/products', ProdutoController.list)
+
 
 
 routes.use(auth)
 routes.post('/likes/:produto_id', FavoritoController.create)
 routes.delete('/likes/:id', FavoritoController.delete)
+routes.post('/users/imgperfil', parserUser.single('imgperfil'), ImagensController.uploadPerfil)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.delete)
 
 routes.use(root)
 routes.get('/users', UserController.list)
-routes.post('/products',parserProdutos.fields([
+routes.post('/products',ProdutoController.create)
+routes.post('/products/imgproducts/:id',parserProdutos.fields([
     { name: 'img1', maxCount: 1 }, 
     { name: 'img2', maxCount: 1 },
     { name: 'img3', maxCount:1}
-]), ProdutoController.create)
-routes.put('/products/:id',parserProdutos.fields([
-    { name: 'img1', maxCount: 1 }, 
-    { name: 'img2', maxCount: 1 },
-    { name: 'img3', maxCount:1}
-]), ProdutoController.update)
+]),  ImagensController.uploadProduto)
+
+routes.put('/products/:id', ProdutoController.update)
 routes.delete('/products/:id', ProdutoController.delete)
 
 
