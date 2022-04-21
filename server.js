aa4ec0535310afdea70b21d2ae51f9a614998577
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-21 15:58:41 -0300

    chore: database setup

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 const app = require('./app')
-const port = process.env.APIPORT || 3333
-app.listen(port || 3333, ()=>console.log('listening on PORT '+port))
+const port = process.env.PORT || 3333
+app.listen(port, ()=>console.log('listening on PORT '+port))
