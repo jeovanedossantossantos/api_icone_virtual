e8084723266eba4dca29c5fe8f2578810e53809b
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-07-19 11:37:21 -0300

    chore: finalizing docker settings

--- src/config/env.js
+++ src/config/env.js
@@ -2,9 +2,9 @@ require('dotenv/config')
 
 module.exports = {
     host: process.env.HOST || process.env.CLEARDB_DATABASE_URL || CLEARDB_DATABASE_URL,
-    db_username: process.env.DBUSER || 'root',
-    password: process.env.PASS || 'A1b3.4J8',
-    database: process.env.DATABASE || 'db_icone_virtual_docker',
+    db_username: process.env.DBUSER,
+    password: process.env.PASS,
+    database: process.env.DATABASE,
     port: 3306,
 
 }
