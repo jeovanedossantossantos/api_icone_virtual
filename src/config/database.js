343ba2a839b037d0bb57ea5d971f063a3b9ef194
jeovanedossantossantos <jeovane.ssantos@outlook.com>
2022-09-05 18:45:35 -0300

    chore: setting docker

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 require('dotenv/config')
 
 const { database, password, host, port, db_username } = require("./env");
-console.log(database, password, host, port, db_username)
+
 
 module.exports = {
   dialect: 'mysql',
   host,
   database,
-  username:db_username,
+  username: db_username,
   password,
   port,
   define: {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const FavoritoController = require('../app/controllers/FavoritoController');
-const ProdutoController  = require('../app/controllers/ProdutoController');
+const ProdutoController = require('../app/controllers/ProdutoController');
 const SessionController = require('../app/controllers/SessionController');
 const UserController = require('../app/controllers/UserController');
 const auth = require('../middlewares/auth');
 const root = require('../middlewares/root');
 const multer = require('multer');
-const {storageProdutos, storageUser} = require('../config/multer');
+const { storageProdutos, storageUser } = require('../config/multer');
 const upload = require('../middlewares/upload');
 const ImagensController = require('../app/controllers/ImagensController');
-const parserUser = multer({storage:storageUser});
-const parserProdutos = multer({storage:storageProdutos})
+const parserUser = multer({ storage: storageUser });
+const parserProdutos = multer({ storage: storageProdutos })
 
 const routes = express.Router();
 
-routes.get('/',(req,res) => {
+routes.get('/', (req, res) => {
     res.send("Olá mundo!")
 })
-routes.post('/session', SessionController.create)
+routes.post('/sessio', SessionController.create)
 routes.post('/users', UserController.create)
 routes.get('/users/:id', UserController.index)
 routes.get('/products/:id', ProdutoController.index)
 routes.get('/products', ProdutoController.list)
-
-
 
 routes.use(auth)
 routes.post('/likes/:produto_id', FavoritoController.create)
@@ -34,12 +32,12 @@ routes.delete('/users/:id', UserController.delete)
 
 routes.use(root)
 routes.get('/users', UserController.list)
-routes.post('/products',ProdutoController.create)
-routes.post('/products/imgproducts/:id',parserProdutos.fields([
-    { name: 'img1', maxCount: 1 }, 
+routes.post('/products', ProdutoController.create)
+routes.post('/products/imgproducts/:id', parserProdutos.fields([
+    { name: 'img1', maxCount: 1 },
     { name: 'img2', maxCount: 1 },
-    { name: 'img3', maxCount:1}
-]),  ImagensController.uploadProduto)
+    { name: 'img3', maxCount: 1 }
+]), ImagensController.uploadProduto)
 
 routes.put('/products/:id', ProdutoController.update)
 routes.delete('/products/:id', ProdutoController.delete)
